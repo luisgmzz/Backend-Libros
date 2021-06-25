e908a2dab407a6bd8e66eca5865bd36ffa3d0028
SoulMaster71 <69698862+SoulMaster71@users.noreply.github.com>
2021-06-25 20:46:08 +0200

    first commit

--- src/database.js
+++ src/database.js
@@ -2,10 +2,21 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "admin",
+    user: process.env.USER,
+    password: process.env.PASSWORD,
     database: "books"
 });
+
+/* 
+TABLE:
+
+id int(11) AI PK 
+name varchar(45) 
+series varchar(45) 
+author varchar(45) 
+publisher varchar(45)
+
+*/
 
 connection.connect(err => { if (err) console.error(err); });
 
